e8455f596fae7215f63f90b79275c7c3647b1c7a
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2023-05-16 18:42:29 +0100

    add extra methods

--- src/Stripe.php
+++ src/Stripe.php
@@ -16,38 +16,94 @@ class Stripe extends Base
 
         return $string;
     }
+
     public function stripeAccountId(): string
     {
-        return 'acct_'.$this->generateRandomString(16);
+        return 'acct_' . $this->generateRandomString(16);
     }
 
     public function stripeApplicationFeeId(): string
     {
-        return 'fee_'.$this->generateRandomString();
+        return 'fee_' . $this->generateRandomString();
     }
 
     public function stripeBalanceTransactionId(): string
     {
-        return 'txn_'.$this->generateRandomString(25);
+        return 'txn_' . $this->generateRandomString(25);
     }
 
     public function stripeChargeId(): string
     {
-        return 'ch_'.$this->generateRandomString();
+        return 'ch_' . $this->generateRandomString();
     }
 
     public function stripeCustomerId(): string
     {
-        return 'cus_'.$this->generateRandomString(14);
+        return 'cus_' . $this->generateRandomString(14);
+    }
+
+    public function stripeDisputeId(): string
+    {
+        return 'dp_' . $this->generateRandomString();
+    }
+
+    public function stripeEventId(): string
+    {
+        return 'evt_' . $this->generateRandomString();
+    }
+
+    public function stripeFileId(): string
+    {
+        return 'file_' . $this->generateRandomString();
+    }
+
+    public function stripeFileLinkId(): string
+    {
+        return 'link_' . $this->generateRandomString();
     }
 
     public function stripeInvoiceId(): string
     {
-        return 'in_'.$this->generateRandomString();
+        return 'in_' . $this->generateRandomString();
     }
 
     public function stripeInvoiceItemId(): string
     {
-        return 'ii_'.$this->generateRandomString();
+        return 'ii_' . $this->generateRandomString();
+    }
+
+    public function stripeMandateId(): string
+    {
+        return 'mandate_' . $this->generateRandomString();
+    }
+
+    public function stripePaymentIntentId(): string
+    {
+        return 'pi_' . $this->generateRandomString();
+    }
+
+    public function stripePayoutId(): string
+    {
+        return 'po_' . $this->generateRandomString();
+    }
+
+    public function stripeRefundId(): string
+    {
+        return 're_' . $this->generateRandomString();
+    }
+
+    public function stripeSetupIntentId(): string
+    {
+        return 'seti_' . $this->generateRandomString();
+    }
+
+    public function stripeSetupAttemptId(): string
+    {
+        return 'setatt_' . $this->generateRandomString();
+    }
+
+    public function stripeTokenId(): string
+    {
+        return 'tok_' . $this->generateRandomString();
     }
 }
--- tests/StripeTest.php
+++ tests/StripeTest.php
@@ -28,10 +28,54 @@ it('generates a customer id', function () {
     expect($this->fake->stripeCustomerId())->toStartWith('cus_')->toHaveLength(18);
 });
 
+it('generates a dispute id', function () {
+    expect($this->fake->stripeDisputeId())->toStartWith('dp_')->toHaveLength(27);
+});
+
+it('generates an event id', function () {
+    expect($this->fake->stripeEventId())->toStartWith('evt_')->toHaveLength(28);
+});
+
+it('generates a file id', function () {
+    expect($this->fake->stripeFileId())->toStartWith('file_')->toHaveLength(29);
+});
+
+it('generates a file link id', function () {
+    expect($this->fake->stripeFileLinkId())->toStartWith('link_')->toHaveLength(29);
+});
+
 it('generates an invoice id', function () {
     expect($this->fake->stripeInvoiceId())->toStartWith('in_')->toHaveLength(27);
 });
 
 it('generates an invoice item id', function () {
     expect($this->fake->stripeInvoiceItemId())->toStartWith('ii_')->toHaveLength(27);
+});
+
+it('generates a mandate id', function () {
+    expect($this->fake->stripeMandateId())->toStartWith('mandate_')->toHaveLength(32);
+});
+
+it('generates a payment intent id', function () {
+    expect($this->fake->stripePaymentIntentId())->toStartWith('pi_')->toHaveLength(27);
+});
+
+it('generates a payout id', function () {
+    expect($this->fake->stripePayoutId())->toStartWith('po_')->toHaveLength(27);
+});
+
+it('generates a refund id', function () {
+    expect($this->fake->stripeRefundId())->toStartWith('re_')->toHaveLength(27);
+});
+
+it('generates a setup intent id', function () {
+    expect($this->fake->stripeSetupIntentId())->toStartWith('seti_')->toHaveLength(29);
+});
+
+it('generates a setup attempt id', function () {
+    expect($this->fake->stripeSetupAttemptId())->toStartWith('setatt_')->toHaveLength(31);
+});
+
+it('generates a token id', function () {
+    expect($this->fake->stripeTokenId())->toStartWith('tok_')->toHaveLength(28);
 });

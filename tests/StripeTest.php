a4df5695b14b3c8eb8e06bd11b56b19c6f443b58
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2023-05-16 16:47:17 +0100

    add table to readme and update stripeFeeId() to stripeApplicationFeeId()

--- tests/StripeTest.php
+++ tests/StripeTest.php
@@ -9,7 +9,12 @@ beforeEach(function () {
 });
 
 it('generates an account id', function () {
+    expect($this->fake->stripeInvoiceItemId())->toBeNull();
     expect($this->fake->stripeAccountId())->toStartWith('acct_')->toHaveLength(21);
+});
+
+it('generates an application fee id', function () {
+    expect($this->fake->stripeApplicationFeeId())->toStartWith('fee_')->toHaveLength(28);
 });
 
 it('generates a balance transaction id', function () {
@@ -22,10 +27,6 @@ it('generates a charge id', function () {
 
 it('generates a customer id', function () {
     expect($this->fake->stripeCustomerId())->toStartWith('cus_')->toHaveLength(18);
-});
-
-it('generates a fee id', function () {
-    expect($this->fake->stripeFeeId())->toStartWith('fee_')->toHaveLength(28);
 });
 
 it('generates an invoice id', function () {
